f8bdd7bf25bb2334073e8410f8480de9f0ff73ac
trijoon <triinjoon@gmail.com>
2022-12-15 04:40:51 +0200

    kõik teisendused olemas

--- AARkalkulaator.py
+++ AARkalkulaator.py
@@ -11,13 +11,13 @@ if valik1 == valikud1[0]:
     valikud2 = ["Kümnendsüsteemis arvuks", "Kaheksandsüsteemis arvuks", "Kuueteistkümnendsüsteemis arvuks"]
     valik2 = choicebox("Milleks soovid teisendada?", "Kahend -> ???", valikud2)
 
+
     # Kahendsüsteemis arv kümnendsüsteemis arvuks
     if valik2 == valikud2[0]:
         arv = int(enterbox("Sisesta arv: ", "Kahend -> kümnend"))
-        kaheksand = 0
         kümnend = 0
         i = 0
-        while arv != 0:
+        while arv:
             kümnend = kümnend + (arv % 10) * 2 ** i
             i += 1
             arv = arv // 10
@@ -31,13 +31,12 @@ if valik1 == valikud1[0]:
         kaheksand = 0
         kümnend = 0
         i = 0
-        while arv != 0:
+        while arv:
             kümnend = kümnend + (arv % 10) * 2 ** i
             i += 1
             arv = arv // 10
-
         i = 1
-        while kümnend != 0:
+        while kümnend:
             kaheksand = kaheksand + (kümnend % 8) * i
             kümnend = kümnend // 8
             i *= 10
@@ -45,10 +44,17 @@ if valik1 == valikud1[0]:
         teisendus = msgbox("Vastus: " + str(kaheksand), "Kahend -> kaheksand")
 
 
-    # Kahendsüsteemisärv kuueteistkümnendsüsteemis arvuks
+    # Kahendsüsteemisarv kuueteistkümnendsüsteemis arvuks
     elif valik2 == valikud2[2]:
         arv = int(enterbox("Sisesta arv: ", "Kahend -> kuueteistkümnend"))
+        kümnend = 0
+        i = 0
+        while arv:
+            kümnend = kümnend + (arv % 10) * 2 ** i
+            i += 1
+            arv = arv // 10
 
+        teisendus = msgbox("Vastus: " + hex(kümnend)[2:].upper(), "Kahend -> kuueteistkümnend")
 
 
 # Kümnendsüsteemis arv
@@ -61,7 +67,6 @@ elif valik1 == valikud1[1]:
     def kümnend_kahend(arv):
         return "{0:b}".format(int(arv))
 
-
     if valik2 == valikud2[0]:
         arv = int(enterbox("Sisesta arv: ", "Kümnend -> kahend"))
         if arv >= 1:
@@ -69,41 +74,42 @@ elif valik1 == valikud1[1]:
 
         teisendus = msgbox("Vastus: " + str(arv), "Kümnend -> kahend")
 
-    # Kümnendsüsteemis arv Kaheksandsüsteemis arvuks
-    if valik2 == valikud2[1]:
-        arv2 = int(enterbox("Sisesta arv: ", "Kümnend -> kaheksand"))
-        vastus = 0
-        ctr = 0
-        kümnend = arv2
 
-        while (kümnend > 0):
-            vastus += ((kümnend % 8) * (10 ** ctr))
+    # Kümnendsüsteemis arv Kaheksandsüsteemis arvuks
+    elif valik2 == valikud2[1]:
+        arv = int(enterbox("Sisesta arv: ", "Kümnend -> kaheksand"))
+        vastus = 0
+        i = 0
+        kümnend = arv
+        while kümnend > 0:
+            vastus += ((kümnend % 8) * (10 ** i))
             kümnend = int(kümnend / 8)
-            ctr += 1
+            i += 1
 
         teisendus = msgbox("Vastus: " + str(vastus), "Kümnend -> kaheksand")
 
-    # Kümnendsüsteemis arv Kuueteistkümnend süsteemis arvuks
-    teisendus_tabel = {0: "0", 1: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8",
+
+    # Kümnendsüsteemis arv Kuueteistkümnendsüsteemis arvuks
+    teisendus_tabel = {0: "0", 1: "2", 2: "", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8",
                        9: "9", 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
 
-
-    def kümnend_kuueteistkümnend(arv3):
-        kuueteistkümnend = " "
-        while (arv3 > 0):
-            jääk = arv3 % 16
-            kuueteistkümnend = teisendus_tabel[jääk] + kuueteistkümnend
-            arv3 = arv3 // 16
+    def kümnend_kuueteistkümnend(arv):
+        kuueteistkümnend = ""
+        while arv > 0:
+            jääk = arv % 16
+            kuueteistkümnend += teisendus_tabel[jääk]
+            arv = arv // 16
 
         return kuueteistkümnend
 
 
     if valik2 == valikud2[2]:
-        arv3 = int(enterbox("Sisesta arv: ", "Kümnend -> kuueteistkümnend"))
-        if arv3 >= 1:
-            arv3 = kümnend_kuueteistkümnend(arv3)
-
-        teisendus = msgbox("Vastus: " + str(arv3), "Kümnend -> kuueteistkümnend")
+        arv = int(enterbox("Sisesta arv: ", "Kümnend -> kuueteistkümnend"))
+        teisendus = msgbox("Vastus: " + hex(arv)[2:].upper(), "Kümnend -> kuueteistkümnend")
+        # if arv >= 1:
+        #     arv = kümnend_kuueteistkümnend(arv)
+        #
+        # teisendus = msgbox("Vastus: " + str(arv), "Kümnend -> kuueteistkümnend")
 
 
 
@@ -112,6 +118,7 @@ elif valik1 == valikud1[2]:
     valikud2 = ["Kahendsüsteemis arvuks", "Kümnendsüsteemis arvuks", "Kuueteistkümnendsüsteemis arvuks"]
     valik2 = choicebox("Milleks soovid teisendada?", "Kaheksand -> ???", valikud2)
 
+
     # Kaheksandsüsteemis arv kahendsüsteemis arvuks
     if valik2 == valikud2[0]:
         arv = int(enterbox("Sisesta arv: ", "Kaheksand -> kahend"))
@@ -119,12 +126,11 @@ elif valik1 == valikud1[2]:
         kümnend = 0
         i = 1
         j = 1
-        while (arv):
+        while arv:
             kümnend += (arv % 10) * i
             i *= 8
             arv //= 10
-
-        while (kümnend):
+        while kümnend:
             kahend += (kümnend % 2) * j
             j *= 10
             kümnend //= 2
@@ -135,11 +141,10 @@ elif valik1 == valikud1[2]:
     # Kaheksandsüsteemisarv kümnendsüsteemis arvuks
     elif valik2 == valikud2[1]:
         arv = int(enterbox("Sisesta arv: ", "Kaheksand -> kümnend"))
-        kahend = 0
         kümnend = 0
         i = 1
         j = 1
-        while (arv):
+        while arv:
             kümnend += (arv % 10) * i
             i *= 8
             arv //= 10
@@ -150,7 +155,15 @@ elif valik1 == valikud1[2]:
     # Kaheksandsüsteemis arv kuueteistkümnendsüsteemis arvuks
     elif valik2 == valikud2[2]:
         arv = int(enterbox("Sisesta arv: ", "Kaheksand -> kuueteistkümnend"))
+        kümnend = 0
+        i = 1
+        j = 1
+        while arv:
+            kümnend += (arv % 10) * i
+            i *= 8
+            arv //= 10
 
+        teisendus = msgbox("Vastus: " + hex(kümnend)[2:].upper(), "Kaheksand -> kuueteistkümnend")
 
 
 
@@ -158,3 +171,29 @@ elif valik1 == valikud1[2]:
 elif valik1 == valikud1[3]:
     valikud2 = ["Kahendsüsteemis arvuks", "Kümnendsüsteemis arvuks", "Kaheksandsüsteemis arvuks"]
     valik2 = choicebox("Milleks soovid teisendada?", "Kuueteistkümnend -> ???", valikud2)
+
+
+    # Kuueteistkümnendsüsteemis arv kahendsüsteemis arvuks
+    if valik2 == valikud2[0]:
+        arv = enterbox("Sisesta arv: ", "Kuueteistkümnend -> kahend")
+        teisendus = msgbox("Vastus: " + bin(int(arv, 16))[2:], "Kuueteistkümnend -> kahend")
+
+
+    # Kuueteistkümnendsüsteemis arv kümnendsüsteemis arvuks
+    elif valik2 == valikud2[1]:
+        arv = enterbox("Sisesta arv: ", "Kuueteistkümnend -> kümnend")
+        teisendus = msgbox("Vastus: " + str(int(arv, 16)), "Kuueteistkümnend -> kümnend")
+
+
+    # Kuueteistkümnendüsteemis arv kaheksandsüsteemis arvuks
+    elif valik2 == valikud2[2]:
+        arv = enterbox("Sisesta arv: ", "Kuueteistkümnend -> kaheksand")
+        vastus = 0
+        i = 0
+        kümnend = int(arv, 16)
+        while kümnend > 0:
+            vastus += ((kümnend % 8) * (10 ** i))
+            kümnend = int(kümnend / 8)
+            i += 1
+
+        teisendus = msgbox("Vastus: " + str(vastus), "Kuueteistkümnend -> kaheksand")
